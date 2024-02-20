b4a963c1e1d4755ce221f3061ac476bf1935a4ca
Carlos Ordaz <carlos.ordaz@acomo.com.pe>
2024-02-20 01:03:38 -0500

    bb

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 app.use(express.json());
 const port = process.env.PORT || 4000;
 
-app.use(cors({ origin: ['https://cultura-general-web.vercel.app'] }));
+app.use(cors({ origin: ['https://cultura-general-web.vercel.app', 'http://localhost:3000'] }));
 
 app.get('/', (req, res) => {
     res.send('v.1.0.13')
